073bdbe3fbea13a9b6b6cf8a2baad9a06bef6aef
CWashington98 <43585651+CWashington98@users.noreply.github.com>
2022-04-22 12:35:49 -0400

    contact page htmldone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeroComponent,
+    ContactComponent,
+    AuthComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
